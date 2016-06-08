d52afbde137361592dfeed671091c3f0d3816ca1
Ekaterina Tcareva <ek.tcareva@gmail.com>
2016-06-07 19:23:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -609,7 +609,7 @@ d3.csv("datasetfor2014.csv", function (data) {
           //   return d.data.age;
             return targetType2(d.data.age);
           });
-          .style("font-size", "16");
+         // .style("font-size", "16");
 
 });
             
@@ -651,7 +651,7 @@ d3.csv("datasetfor2014.csv", function (data) {
           //   return d.data.age;
             return targetType2(d.data.age);
           });
-          .style("font-size", "16");
+          //.style("font-size", "16");
 });  
             })  
             .on("mouseout", function (d) {
